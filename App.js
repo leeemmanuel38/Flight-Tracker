542c0a22264ce7017c623ee4ba586b1d7a68fd26
jmayo93 <60381665+jmayo93@users.noreply.github.com>
2020-04-19 13:42:10 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+
+import History from './Components/History'; 
+import Home from './Components/Home'; 
+import Profile from './Components/Profile';
+import Dashboard from './Components/Dashboard'; 
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Results from './Components/Results';
+import NotFound from './Components/NotFound';
+
+export default class App extends Component {
+
+    render() {
+        //jsx
+        return (
+          <Router history={History}>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/profile" component={Profile}/>
+              <Route path="/dashboard" component={Dashboard}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/signup" component={Signup}/>
+              <Route path="/results" component={Results}/>
+              <Route path="/404" component={NotFound}/>
+            </Switch>
+          </Router>
+            
+        );
+    }
+}
