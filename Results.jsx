e358ba0a0d9b7156c9e526ec08bf2584323d08d3
Emmanuel Lee <50498531+leeemmanuel38@users.noreply.github.com>
2020-04-23 14:06:07 -0400

    Add files via upload

--- Results.jsx
+++ Results.jsx
@@ -1,104 +1,97 @@
-import React, { Component } from 'react'; 
-import axios from 'axios'; 
-import {} from 'react-router-dom'; 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import { render } from '@testing-library/react';
-import Drawer from './ResultsDrawer'; 
-import Toggle from './Toggle';
+import React, { Component } from 'react';
+import Toggle from './Toggle'; 
+import Drawer from './ResultsDrawer';
+import '../App.css';
 
+function seacrchingFor(word){
+    return function(search){
+        return search.name.toLowerCase().includes(word.toLowerCase()) || search.code.toLowerCase().includes(word.toLowerCase());
+    }
+}
 
-const style = {
-    margin: 15,
-   };
-  
-   export default class Results extends Component {
-  
-      render(){
-          return(
-            <div class="wrap">
-              <MuiThemeProvider>
-              <Drawer/>
-                <div>
-                    <div class="container">
-                        <div class="row">
-                            <div class="form-group col-md-12">
-                            </div>
-                            <div class="form-group col-md-12">
-                            </div>            
-                        </div>
-                    </div> 
-                  <p class="text-center">
-                  <p class = "text-white"><p class="font-weight-bold"><h1>Available Flights (WIP)</h1></p></p>
-                  </p>
-                <div>
-                <div class="container">
-                    <div class="row">
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                         <div class="form-group col-md-12">
-                         </div>
-                    </div>
-                </div>
-    
-                <span class="d-block p-2 bg-dark text-white">
-                <div class="container">
-                    <div class="row"> 
-                        <div class="form-group col-md-2">
-                        <small><h6>Departing from: Albany</h6></small>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <h6>Arriving in: Spain</h6>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <h6>Price: 400</h6>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <h6>Date: 11/12/13</h6>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <h6>Class: First</h6>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <small><small><Toggle text="Flight Added!" /></small></small>
-                        </div>
+export default class Results extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items: [], 
+            isLoaded: false,
+            word: '', 
+        }
+        this.searchandler = this.searchandler.bind(this);
+    }
+
+    searchandler(event){
+        this.setState({word: event.target.value})
+    }
+
+    componentDidMount(){
+        fetch('https://gist.githubusercontent.com/tdreyno/4278655/raw/7b0762c09b519f40397e4c3e100b097d861f5588/airports.json')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true, 
+                    items: json,  
+            })
+        });
+    }
+    render() {
+
+        var { isLoaded, } = this.state; 
+        if(!isLoaded){
+            return <div class="text-center"><h3>Loading...</h3></div>
+        }
+
+        else {
+            const {word, items} = this.state; 
+            return(
+                <div class="ui centered aligned grid">
+                    <Drawer/>
+                    <div class="wrap"> 
+                        <h1 class="text-right">Available Flights (WIP)</h1>
+                        <form class="form-group text-center">
+                         
+                            <input type="text"
+                                onChange={this.searchandler} 
+                                value={word}
+                            />
                             
-                        </div> 
-                        </div>
-                        </span>
-                        <br/>
-                        <br/>
-                        <div class="container">
-                            <div class="row"> 
-                                 <div class="form-group col-md-6">
-                                 <button type="button" class="btn btn-primary">Submit</button>
-                                </div>
-                                </div>
-                                </div>
-                        
-                        
-                               
-                    </div> 
-                </div>         
-             </MuiThemeProvider>
-             <br/>
-             <br/>
-             <br/>
-             <br/>
-             <br/>
-        </div>
-    );
- }
+                        </form>
+                        <ul>
+                            {items.filter(seacrchingFor(word)).map(data => 
+                                    <div className="resultForms">
+                                        <li>
+                                            <span class="d-block p-2">
+                                                <div class="container">
+                                                    <div class="row"> 
+                                                        <div class="form-group col-sm-3">
+                                                            <h6>Airport: {data.name}</h6>
+                                                        </div>
+                                                        <div class="form-group col-sm-3">
+                                                            <h6>City: {data.city}</h6>
+                                                        </div>
+                                                        <div class="form-group col-sm-3">
+                                                            <h6>Country: {data.country}</h6>
+                                                        </div>
+                                                        <div class="form-group col-sm-3">
+                                                        <small><small><Toggle text="Flight Added!" /></small></small>
+                                                        </div>
+                                
+                                                    </div> 
+                                                </div>
+                                            </span>
+                                        </li>
+                                    </div>
+                            )};
+
+                           
+                        </ul>
+                    </div>
+    
+                </div>
+            );
+            
+        }
+    }
+
 }
