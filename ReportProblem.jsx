8aa26325c3583c22c69b21bf4c240fde55274b1e
Jane <30421010+dracarystargaryen@users.noreply.github.com>
2020-05-05 14:48:17 -0400

    Add files via upload

---
+++ ReportProblem.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+
+
+export default class ReportProblem extends React.Component{
+  render(){
+    return (
+<div>
+<html>
+<body>
+
+<h1 style="background-color:LightGreen;">Report a problem</h1>
+<div class="container">
+  <form action="/action_page.php">
+  <div class="row">
+    <div class="col-25">
+      <label for="fname">First Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="lname">Last Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    </div>
+    </input>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="subject">Subject</label>
+    </div>
+    <div class="col-75">
+      <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+    </div>
+  </div>
+  <div class="row">
+    <input type="submit" value="Submit">
+  </div>
+  </form>
+</div>
+
+</body>
+</html>
+
+
+
+  
+
+</body>
+</html>
+</div>
+
+    )
+  }
+}
+
+export default Class 
